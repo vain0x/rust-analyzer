716e6d9bb66b5ae6c50585531bb4f8cc6a2cb24a
vain0x <vain0x@users.noreply.github.com>
2020-06-02 10:13:43 +0000

    deploy: 21132a7a748a70351f5d1ae6d8c51a3a0065013e

---
+++ stdx/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["is_ci",""],["replace",""],["timeit",""],["to_lower_snake_case",""]],"macro":[["eprintln",""],["format_to","Appends formatted string to a `String`."]],"struct":[["SepByBuilder",""]],"trait":[["SepBy",""]]});
